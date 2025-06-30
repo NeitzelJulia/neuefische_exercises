ffd72ae8333c2c8709918dc59dc99b7685e33f69
NeitzelJulia <julia.neitzel86@gmail.com>
2025-06-30 10:23:36 +0200

    11_TDD implement minimal move methods

--- modules/01_java_basics/11_TDD/src/main/java/PlayerCharacter.java
+++ modules/01_java_basics/11_TDD/src/main/java/PlayerCharacter.java
@@ -11,4 +11,16 @@ public class PlayerCharacter {
     public static int[] moveW(){
         return new int[] {0,1};
     }
+
+    public static int[] moveS(){
+        return new int[] {0,-1};
+    }
+
+    public static int[] moveD(){
+        return new int[] {1,0};
+    }
+
+    public static int[] moveA(){
+        return new int[] {-1,0};
+    }
 }
--- modules/01_java_basics/11_TDD/src/test/java/PlayerCharacterTest.java
+++ modules/01_java_basics/11_TDD/src/test/java/PlayerCharacterTest.java
@@ -11,13 +11,32 @@ class PlayerCharacterTest {
 
     @Test
     void testGetY_InitialPosition() {
-        assertEquals(0,PlayerCharacter.getX());
+        assertEquals(0,PlayerCharacter.getY());
     }
 
     @Test
     void testMoveW() {
         int[] expected = {0,1};
         assertArrayEquals(expected, PlayerCharacter.moveW());
+    }
+
+    @Test
+    void testMoveS() {
+        int[] expected = {0,-1};
+        assertArrayEquals(expected, PlayerCharacter.moveS());
+    }
+
+
+    @Test
+    void testMoveD() {
+        int[] expected = {1,0};
+        assertArrayEquals(expected, PlayerCharacter.moveD());
+    }
+
+    @Test
+    void testMoveA() {
+        int[] expected = {-1,0};
+        assertArrayEquals(expected, PlayerCharacter.moveA());
     }
 
 
