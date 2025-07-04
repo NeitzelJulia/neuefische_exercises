95c256ff92c05a10b446f2ca6d1a01f8d3429341
NeitzelJulia <julia.neitzel86@gmail.com>
2025-07-04 15:52:04 +0200

    05_interface

---
+++ modules/02_oop/05_interface/src/main/java/MediaController.java
@@ -0,0 +1,7 @@
+public class MediaController {
+
+    public void playMedia(Playable playable) {
+        playable.play();
+    }
+
+}
---
+++ modules/02_oop/05_interface/src/main/java/VideoPlayer.java
@@ -0,0 +1,5 @@
+public class VideoPlayer implements Playable{
+    public void play() {
+        System.out.println("Video playing...");
+    }
+}
