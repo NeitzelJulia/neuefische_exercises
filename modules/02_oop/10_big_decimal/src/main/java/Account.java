20a1eaeb7fd12435432ecea8c8b3f9fef9599585
NeitzelJulia <julia.neitzel86@gmail.com>
2025-07-08 15:18:29 +0200

    10_big_decimal account can have multiple clients

--- modules/02_oop/10_big_decimal/src/main/java/Account.java
+++ modules/02_oop/10_big_decimal/src/main/java/Account.java
@@ -1,23 +1,36 @@
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
 
 public class Account {
 
     private final String accountNo;
     private BigDecimal balance;
-    private final Client client;
+    private final List<Client> clients;
+
+    Account(String accountNo, List<Client> clients) {
+        this.accountNo = accountNo;
+        this.clients = List.copyOf(clients);
+        balance = BigDecimal.ZERO;
+    }
 
     Account(String accountNo, Client client) {
-        this.accountNo = accountNo;
-        this.client = client;
-        balance = new BigDecimal(0);
+        this(accountNo, List.of(client));
     }
 
     public void deposit(BigDecimal amount) {
+        Objects.requireNonNull(balance, "balance is null");
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException();
+        }
         balance = balance.add(amount);
     }
 
     public void withdraw(BigDecimal amount) {
+        Objects.requireNonNull(balance, "balance is null");
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException();
+        }
         balance = balance.subtract(amount);
     }
 
@@ -29,8 +42,8 @@ public class Account {
         return balance;
     }
 
-    public Client getClient() {
-        return client;
+    public List<Client> getClients() {
+        return clients;
     }
 
     @Override
@@ -38,7 +51,7 @@ public class Account {
         return "Account{" +
                 "accountNo='" + accountNo + '\'' +
                 ", balance=" + balance +
-                ", client=" + client +
+                ", clients=" + clients +
                 '}';
     }
 
@@ -46,11 +59,11 @@ public class Account {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Account account = (Account) o;
-        return Objects.equals(accountNo, account.accountNo) && Objects.equals(balance, account.balance) && Objects.equals(client, account.client);
+        return Objects.equals(accountNo, account.accountNo) && Objects.equals(balance, account.balance) && Objects.equals(clients, account.clients);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(accountNo, balance, client);
+        return Objects.hash(accountNo, balance, clients);
     }
 }
--- modules/02_oop/10_big_decimal/src/main/java/BankService.java
+++ modules/02_oop/10_big_decimal/src/main/java/BankService.java
@@ -1,23 +1,39 @@
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class BankService {
 
-    private Map<String, Account> accounts = new HashMap<>();
+    private final Map<String, Account> accounts = new ConcurrentHashMap<>();
     private final AtomicLong nextAccountNo = new AtomicLong(10000001);
 
-    public String openAccount(Client client) {
+    public String openAccount(List<Client> clients) {
         String accountNo = String.format("%010d", nextAccountNo.getAndIncrement());
-        Account account = new Account(accountNo, client);
+        Account account = new Account(accountNo, clients);
         accounts.put(accountNo, account);
         return accountNo;
     }
 
+    public String openAccount(Client client) {
+        return openAccount(List.of(client));
+    }
+
     public void transfer(String from, String to, BigDecimal amount) {
-        accounts.get(from).withdraw(amount);
-        accounts.get(to).deposit(amount);
+        Account fromAcc = accounts.get(from);
+        Account toAcc = accounts.get(to);
+
+        if (fromAcc == null || toAcc == null) {
+            throw new IllegalArgumentException("invalid account no");
+        }
+
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("invalid amount");
+        }
+
+        fromAcc.withdraw(amount);
+        toAcc.deposit(amount);
     }
 
 }
